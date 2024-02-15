3679f0fee1ac55742526c98069499b4069583cd8
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 22:12:56 +0530

    commit

---
+++ Operator/Arithmetic.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int a=5;
+        int b=15;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
