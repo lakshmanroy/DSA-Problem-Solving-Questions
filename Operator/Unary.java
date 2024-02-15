cbc8934dcb4ad61041714dc67017ed0a8cbb686b
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 23:08:05 +0530

    commit

---
+++ Operator/Unary.java
@@ -0,0 +1,18 @@
+public class Unary {
+    public static void main(String[] args) {
+        int a=10;
+        int b= ++a;
+        System.out.println(a);
+        System.out.println(b);
+
+        // int x=10;
+        // int y=x++;
+        // System.out.println(x);
+        // System.out.println(y);
+
+        // int a=5;
+        // int b=--a;
+        // System.out.println(a);
+        // System.out.println(b);
+    }
+}
