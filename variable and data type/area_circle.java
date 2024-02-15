0e2a8ff9fd5c16354d1df7664ba6ed9278270a67
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 21:53:50 +0530

    commit

---
+++ variable and data type/area_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class area_circle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int rad=sc.nextInt();
+        float area=3.13f*rad*rad;
+        System.out.println(area);
+    }
+    
+}
