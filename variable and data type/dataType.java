b4c7f654e9eb9ffd3e254a59eff9b4fec54d13ac
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 21:36:39 +0530

    commit

---
+++ variable and data type/dataType.java
@@ -0,0 +1,15 @@
+public class dataType {
+    public static void main(String[] args) {
+        int a=4;
+        float b=10.5f;
+        double c=23.44D;
+        boolean d=true;
+        long e=345676553L;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
