ab93df6e58f2c7c9f831bfd427a2359abf2abbf1
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 23:18:07 +0530

    commit

---
+++ Operator/AssignmentOperator.java
@@ -0,0 +1,10 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int A=10;
+        A+=2;
+        System.out.println(A);
+        int B=5;
+        B*=5;
+        System.out.println(B);
+    }
+}
