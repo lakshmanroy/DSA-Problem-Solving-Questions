3e9e891f1b1a81ac27900188172dae3cbe946641
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 18:46:24 +0530

    updated

---
+++ Array/solution1.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class solution1 {
+    
+    public static void main(String[] args) {
+        int number[]={1,2,3,4};
+       System.out.println(number[0]);
+       System.out.println(number[1]);
+       System.out.println(number[2]);
+       System.out.println(number[3]);
+    }
+}
