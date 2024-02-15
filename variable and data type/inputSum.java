f7c35ac39ca56b0bbf32c9bf9bd21c919de0bf87
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 21:48:57 +0530

    commit

---
+++ variable and data type/inputSum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class inputSum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        int a= sc.nextInt();
+        System.out.println("Enter your second number:");
+        int b= sc.nextInt();
+
+        int sum= a+b;
+        System.out.println("sum of number:"+sum);
+    }
+    
+}
