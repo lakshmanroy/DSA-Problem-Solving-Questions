2a39c5aae22bcfcf0bd31f106b107339b782fe5f
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 21:31:57 +0530

    second commit

---
+++ variable and data type/variable.java
@@ -0,0 +1,14 @@
+public class variable {
+   public static void main(String[] args) {
+    int a=5;
+    int b=10;
+
+    System.out.println(a);
+    System.out.println(b);
+
+    String name="Lakshman";
+    System.out.println(name);
+    a=b;
+    System.out.println(a);
+   } 
+}
