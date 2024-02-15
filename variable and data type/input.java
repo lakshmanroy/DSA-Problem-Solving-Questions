f1ed3d5ff923b0ccd72f7c07505c1fe4f7eca8d4
Lakshman Roy <lkrd5115@gmail.com>
2024-02-15 21:44:36 +0530

    commit

---
+++ variable and data type/input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int number= sc.nextInt();
+        System.out.println(number);
+
+        String name=sc.next();
+        System.out.println(name);
+
+        float price=sc.nextFloat();
+        System.out.println(price);
+    }
+}
