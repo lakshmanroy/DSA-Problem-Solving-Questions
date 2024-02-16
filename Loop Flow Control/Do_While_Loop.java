5f5c6b9e8c18be8b58fdac1f4b7c5921746bffbd
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 23:35:27 +0530

    commit

---
+++ Loop Flow Control/Do_While_Loop.java
@@ -0,0 +1,10 @@
+public class Do_While_Loop {
+    public static void main(String[] args) {
+        int counter=1;
+        do{
+            System.out.println("Hello world");
+            counter++;
+        }
+        while(counter<=10);
+    }
+}
