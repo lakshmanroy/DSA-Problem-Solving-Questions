f827dbd042ccfee0f4ca24801f3b6f5b78247516
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 21:14:19 +0530

    commit

---
+++ condition statements/LargestNumber.java
@@ -0,0 +1,11 @@
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a=5;
+        int b=4;
+        if (a>b) {
+            System.out.println("A is largest number");  
+        }else{
+            System.out.println("B is largest number");
+        }
+    }
+}
