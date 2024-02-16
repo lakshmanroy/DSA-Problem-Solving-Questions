571d21d7c51d22ee87ac7de9b59470baeb795bbb
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 21:40:08 +0530

    commit

---
+++ condition statements/Largest_3_of.java
@@ -0,0 +1,14 @@
+public class Largest_3_of {
+    public static void main(String[] args) {
+        int A=1,B=3,C=6;
+        if ((A>=B) && (A>=C)) {
+            System.out.println("largest of A");
+        }
+        else if (B>=C) {
+            System.out.println("largest of B");
+        }
+        else{
+            System.out.println("largest of C");
+        }
+    }
+}
