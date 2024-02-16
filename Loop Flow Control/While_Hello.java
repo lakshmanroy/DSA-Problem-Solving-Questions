575b3c673cf4d59b70e34dbdad75467997aa4f97
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 22:37:23 +0530

    commit

---
+++ Loop Flow Control/While_Hello.java
@@ -0,0 +1,10 @@
+public class While_Hello {
+    public static void main(String[] args) {
+        int counter=0;
+        while (counter<=10) {
+            System.out.println("Hello world");
+            counter++;  
+        }
+        System.out.println();
+    }
+}
