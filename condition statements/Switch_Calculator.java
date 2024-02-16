1d10928a130799d03fcfb7d4f6632cef58260311
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 22:08:39 +0530

    commit

---
+++ condition statements/Switch_Calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Switch_Calculator {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a=sc.nextInt();
+        System.out.println("Enter b:");
+        int b=sc.nextInt();
+        System.out.println("Enter your operator:");
+        int operator= sc.next().charAt(0);
+        switch (operator) {
+                     case '+':System.out.println(a+b);
+                     break;
+                     case '-':System.out.println(a-b);
+                     break;
+                     case '*':System.out.println(a*b);
+                     break;
+                     case '/':System.out.println(a/b);
+                     break;
+                     case '%':System.out.println(a%b);
+                     break;
+                     default:System.out.println("wrong operator");
+        }
+    }
+}
