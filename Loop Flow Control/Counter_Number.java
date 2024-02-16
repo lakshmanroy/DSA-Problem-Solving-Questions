d630ba0c0a08ee663e0147320bbe40f83a358df9
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 22:44:47 +0530

    commit

---
+++ Loop Flow Control/Counter_Number.java
@@ -0,0 +1,10 @@
+public class Counter_Number {
+   public static void main(String[] args) {
+    int counter=1;
+    while (counter<=10) {
+        System.out.println(counter);
+        counter++;  
+    }
+    System.out.println();
+   } 
+}
