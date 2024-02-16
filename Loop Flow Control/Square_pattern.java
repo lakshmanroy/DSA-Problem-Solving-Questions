aedda53baf6a03dd75e52fe0e51b87c90cb6b8d1
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 23:11:30 +0530

    commit

---
+++ Loop Flow Control/Square_pattern.java
@@ -0,0 +1,13 @@
+public class Square_pattern {
+    public static void main(String[] args) {
+        // for(int i=1;i<=4;i++){
+        //     System.out.println("****");
+        // }
+
+        int line=1;
+        while (line<=4) {
+            System.out.println("****");
+            line++;
+        }
+    }
+}
