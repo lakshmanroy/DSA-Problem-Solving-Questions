8aa1cbc28755c11850c9b9b5e9f65427232d39f4
Lakshman Roy <lkrd5115@gmail.com>
2024-02-16 23:31:57 +0530

    commit

---
+++ Loop Flow Control/Reverse_Number.java
@@ -0,0 +1,12 @@
+public class Reverse_Number {
+   public static void main(String[] args) {
+    int n=10899;
+    int rev=0;
+    while (n>0) {
+        int lastDigit=n%10;
+        rev=(rev*10)+lastDigit;
+        n=n/10;
+    }
+    System.out.println(rev);
+   } 
+}
