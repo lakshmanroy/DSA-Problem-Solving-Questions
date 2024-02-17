e98adad59b655b744c8807a4dc09dd35c6bb22dd
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 22:55:37 +0530

    commit

--- Functions and Methods/Function_Overloading.java
+++ Functions and Methods/Function_Overloading.java
@@ -5,8 +5,12 @@ public class Function_Overloading {
     public static int sum(int a,int b,int c){
         return a+b+c;
     }
+    public static float sum(float a,float b){
+        return a+b;
+    }
     public static void main(String[] args) {
         System.out.println(sum(2, 7));
         System.out.println(sum(3,8, 15));
+        System.out.println(sum(3.2f,4.8f));
     }
 }
