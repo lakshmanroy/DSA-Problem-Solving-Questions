23c60c5e0f2e111379f6d04ed2f527042592094f
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 23:15:02 +0530

    commit

--- Functions and Methods/Prime_Number.java
+++ Functions and Methods/Prime_Number.java
@@ -1,14 +1,28 @@
 public class Prime_Number {
+    // public static boolean isPrime(int n){
+    //     boolean isPrime=true;
+    //     for(int i=2;i<=n-1;i++){
+    //         if (n%i==0) {
+    //             isPrime=false;
+    //         }
+    //     }
+    //     return isPrime;
+    // }
+
     public static boolean isPrime(int n){
-        boolean isPrime=true;
-        for(int i=2;i<=n-1;i++){
+       if (n==2) {
+        return true;
+       }
+        // boolean isPrime=true;
+        for(int i=2;i<=Math.sqrt(n);i++){
             if (n%i==0) {
-                isPrime=false;
+                return false; 
             }
-        }
-        return isPrime;
+        }return true;
     }
     public static void main(String[] args) {
-       System.out.println( isPrime(7));
+    //    System.out.println( isPrime(7));
+
+    System.out.println(isPrime(5));
     }
 }
