78a10423335101e5f8c8e2347eb130b4506acf2a
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 21:53:34 +0530

    commit

---
+++ Functions and Methods/MethodHello.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MethodHello {
+    public static void HelloWorld(){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+    public static void calculateSum(int a,int b){
+        int sum= a+b;
+        System.out.println("Sum is :" +sum);
+    }
+   public static void main(String[] args) {
+    // HelloWorld();
+
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+
+    calculateSum(a, b);
+   } 
+}
