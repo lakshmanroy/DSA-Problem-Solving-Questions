4668c1b6670fdf6c7305f34e1811473aac0aa9e2
Lakshman Roy <lkrd5115@gmail.com>
2024-02-17 22:24:06 +0530

    commit

--- Functions and Methods/Swap_Function.java
+++ Functions and Methods/Swap_Function.java
@@ -4,6 +4,11 @@ public class Swap_Function {
         a=b;
         b=temp;
     }
+
+    public static int multiply(int x,int y){
+        int product=x*y;
+        return product;
+    }
     public static void main(String[] args) {
         int a=5;
         int b=10;
@@ -11,6 +16,17 @@ public class Swap_Function {
         Swap(a, b);
         System.out.println("a=" +a);
         System.out.println("b=" +b);
+
+        System.out.println("__________________________");
+        
+        int x=10;
+        int y=5;
+
+        int prod=multiply(x, y);
+        System.out.println("multiply is:" +prod);
+
+        prod=multiply(10, 20);
+        System.out.println("multiply is:" +prod);
         
     }
 }
