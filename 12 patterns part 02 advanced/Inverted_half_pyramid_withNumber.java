adb5e7f45483a39f380540f4ae5d3944f764a52a
Lakshman Roy <lkrd5115@gmail.com>
2024-02-18 22:07:16 +0530

    commit

---
+++ 12 patterns part 02 advanced/Inverted_half_pyramid_withNumber.java
@@ -0,0 +1,14 @@
+public class Inverted_half_pyramid_withNumber {
+    public static void inverted_half_pyramid_WithNumber(int n){
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            System.out.print(j+ "");
+        }
+        System.out.println();
+     }
+
+    }
+    public static void main(String[] args) {
+        inverted_half_pyramid_WithNumber(5);
+    }
+}
