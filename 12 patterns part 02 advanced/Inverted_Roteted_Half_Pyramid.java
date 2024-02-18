f29399ad6a67c697ce068b803efa2a87159199dc
Lakshman Roy <lkrd5115@gmail.com>
2024-02-18 21:56:18 +0530

    commit

---
+++ 12 patterns part 02 advanced/Inverted_Roteted_Half_Pyramid.java
@@ -0,0 +1,16 @@
+public class Inverted_Roteted_Half_Pyramid {
+    public static void  halfPyramid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        halfPyramid(5);
+    }
+}
