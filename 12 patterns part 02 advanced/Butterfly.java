f491c8418d08b04bdcfcde0804e53776c91ade9f
Lakshman Roy <lkrd5115@gmail.com>
2024-02-18 23:10:38 +0530

    commit

--- 12 patterns part 02 advanced/Butterfly.java
+++ 12 patterns part 02 advanced/Butterfly.java
@@ -1,5 +1,6 @@
 public class Butterfly {
     public static void Butterflys(int n){
+    //1st half
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
