f222eaf7aebf0f7b39702a9f554f84f3cc150c67
Lakshman Roy <lkrd5115@gmail.com>
2024-02-19 23:15:13 +0530

    commit

---
+++ Arrays/LargestArray.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class LargestArray {
+    public static int largestNumber(int number[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i=0;i<number.length;i++){
+            if (largest<number[i]) {
+                largest=number[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int number[]={2,4,10,6,8};
+        System.out.println("largest number is:" +largestNumber(number));
+    }
+}
