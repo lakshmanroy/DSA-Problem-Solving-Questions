3e0e24309e47c26e55180fb37d1c92b48250b139
Lakshman Roy <lkrd5115@gmail.com>
2024-02-22 23:01:19 +0530

    commit

--- Arrays/Pairse.java
+++ Arrays/Pairse.java
@@ -2,13 +2,16 @@ package Arrays;
 
 public class Pairse {
     public static void printPairs(int number[]){
+        int tp=0;
         for(int i=0;i<number.length;i++){
             int curr=number[i];
             for(int j=i+1;j<number.length;j++){
                 System.out.print("(" +curr+ "," +number[j]+ ")");
+                tp++;
             }
             System.out.println();
         }
+        System.out.println("total tp :"+tp);
     }
     public static void main(String[] args) {
         int number[]={2,4,6,8,10};
---
+++ Arrays/SubArray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class SubArray {
+    public static void printSubArray(int number[]){
+        for(int i=0;i<number.length;i++){
+            int start=i;
+            for(int j=i;j<number.length;j++){
+                int end=j;
+                for(int k=start;k<=end;k++){
+                    System.out.print(number[k]+ " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int number[]= {2,4,6,8,10};
+        printSubArray(number);
+        
+    }
+}
