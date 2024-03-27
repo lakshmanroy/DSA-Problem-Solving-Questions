1b7d7b176710bf8ca98ecbd551e9337cd707b086
Lakshman Roy <lkrd5115@gmail.com>
2024-03-27 22:44:09 +0530

    commit

--- Arrays/TrappedRainwater.java
+++ Arrays/TrappedRainwater.java
@@ -22,7 +22,7 @@ public class TrappedRainwater {
     }
     public static void main(String[] args) {
         int height[]= {4,2,0,6,3,2,5};
-        trappedWaters(height);
+       System.out.println(trappedWaters(height));
     }
     
 }
