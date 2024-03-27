3923fa7488fcb19803a2066aa6958305384cd90c
Lakshman Roy <lkrd5115@gmail.com>
2024-03-28 00:02:04 +0530

    commit

---
+++ Arrays/BuyAndSell.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class BuyAndSell {
+    public static int buyAndCellProduct(int prices[]){
+        int buyPrice= Integer.MAX_VALUE;
+        int  maxProfit=0;
+        for(int i=0;i<prices.length;i++){
+            if (buyPrice<prices[i]) {
+                int profit= prices[i]-buyPrice;
+                maxProfit=Math.max(maxProfit,profit);
+            }else{
+                buyPrice= prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[]= {7,1,5,3,6,4};
+
+        System.out.println(buyAndCellProduct(prices));
+    }
+}
