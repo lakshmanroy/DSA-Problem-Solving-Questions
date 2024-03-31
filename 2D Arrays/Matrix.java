8f4c45b4345eddb6f417960dd046263532d6f48d
Lakshman Roy <lkrd5115@gmail.com>
2024-03-31 14:09:26 +0530

    commit

--- 2D Arrays/Matrix.java
+++ 2D Arrays/Matrix.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 import java.util.*;
 
 public class Matrix {
+    public static boolean search(int matrix[][],int key){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if (matrix[i][j]==key) {
+                    System.out.print("found at cell (" +i+ ","+j+ ")");
+                    return true;
+                }
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
     public static void main(String[] args) {
         int matrix[][]= new int[3][3];
         int n=matrix.length, m= matrix[0].length;
@@ -17,5 +29,6 @@ public class Matrix {
             }
             System.out.println();
         }
+        search(matrix,5);
     }
 }
