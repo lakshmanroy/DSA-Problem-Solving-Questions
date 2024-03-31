fa3bd491c4dbf5325789cd97d0bbca6e64a20631
Lakshman Roy <lkrd5115@gmail.com>
2024-03-31 14:34:22 +0530

    commit

---
+++ 2D Arrays/matrixLargest.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class matrixLargest {
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+        Scanner sc = new Scanner(System.in);
+        
+        // Input matrix elements
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        
+        // Find the largest number
+        int largest = matrix[0][0];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+            }
+        }
+        
+        System.out.println("The largest number in the matrix is: " + largest);
+    }
+}
