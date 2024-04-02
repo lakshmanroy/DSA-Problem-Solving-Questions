106779f64f38396dbd0b0c3c5d704ec9ce96ace5
Lakshman Roy <lkrd5115@gmail.com>
2024-04-02 21:20:12 +0530

    commit

--- 2D Arrays/DiagonalSum.java
+++ 2D Arrays/DiagonalSum.java
@@ -1,17 +1,26 @@
 public class DiagonalSum {
     public static int DigonalSums(int matrix[][]){
-        int sum=0;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                if (i==j) {
-                    sum+= matrix[i][j];
-                }
-                else if (i+j==matrix.length-1) {
-                    sum+=matrix[i][j];
+         int sum=0;
+        // for(int i=0;i<matrix.length;i++){
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if (i==j) {
+        //             sum+= matrix[i][j];
+        //         }
+        //         else if (i+j==matrix.length-1) {
+        //             sum+=matrix[i][j];
                     
-                }
+        //         }
+        //     }
+        // }
+        for(int i=0;i<matrix.length;i++){
+            //pd
+            sum+= matrix[i][i];
+            //sd
+            if (i !=matrix.length-1-i) {
+                sum+= matrix[i][matrix.length-i-1];
             }
         }
+        
         return sum;
     }
     public static void main(String[] args) {
