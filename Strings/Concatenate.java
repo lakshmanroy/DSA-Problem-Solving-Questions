c08b6b88275371dc8c33fbbfc8c2ffbb9ca9b81f
Lakshman Roy <lkrd5115@gmail.com>
2024-04-03 00:44:03 +0530

    commit

---
+++ Strings/Concatenate.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class Concatenate {
+    public static void main(String[] args) {
+        String firstName= "Lakshman";
+        String secondName= "Riya";
+        String fullName= firstName+ " "+ secondName;
+        System.out.println(fullName);
+
+    }
+}
