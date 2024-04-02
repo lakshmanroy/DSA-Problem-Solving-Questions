169b258e8a94329ca5da0904b98bd827950e9a40
Lakshman Roy <lkrd5115@gmail.com>
2024-04-02 23:52:17 +0530

    commit

---
+++ Strings/Strings.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        char arr[]= {'a','b','c','d'};
+
+        String str= "abcd";
+        String str2= new String("abcd");
+
+        //Strings are IMMUTABLE
+    }
+    
+}
