833bedf7f26f8a15489fa591cf1fcd582a62ab41
Lakshman Roy <lkrd5115@gmail.com>
2024-04-03 00:33:01 +0530

    commit

---
+++ Strings/Length.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class Length {
+    public static void main(String[] args) {
+        String name[]= {"Lakshman","Riya","Vs","Radha"};
+        System.out.println(name.length);
+    }
+    
+}
