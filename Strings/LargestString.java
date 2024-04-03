a41352435a58e8d6966cd559c90d536d03aefdc4
Lakshman Roy <lkrd5115@gmail.com>
2024-04-03 18:26:49 +0530

    commit

---
+++ Strings/LargestString.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class LargestString {
+    public static void main(String[] args) {
+        String fruit[]= {"apple","mango","banana"};
+        String largest= fruit[0];
+        for(int i=1;i<fruit.length;i++){
+            if (largest.compareTo(fruit[i])<0) {
+                largest= fruit[i];
+            }
+        }
+        System.out.println(largest);
+    }
+    
+}
