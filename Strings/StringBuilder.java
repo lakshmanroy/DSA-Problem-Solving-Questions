16bc701ad4ad7c41c7d3a0a81d21185b1e6fb68f
Lakshman Roy <lkrd5115@gmail.com>
2024-04-04 22:45:38 +0530

    commit

---
+++ Strings/StringBuilder.java
@@ -0,0 +1,17 @@
+package Strings;
+import java.util.*;
+
+public class StringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+    }
+
+    private void append(char ch) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'append'");
+    }
+}
