10a39ce95b24fc4edc2a5a59d8aeede74fc2d08f
Lakshman Roy <lkrd5115@gmail.com>
2024-04-05 21:18:32 +0530

    commit

---
+++ bit Manipilation/BitWiseOperator.java
@@ -0,0 +1,11 @@
+
+public class BitWiseOperator {
+    public static void main(String[] args) {
+        //Binary AND &
+        //Binary OR |
+        //Binary XOR 
+        //Binary one's complemnet~
+        //Binay Left Shift<<
+        //Binary Right Shift<<
+    }
+}
