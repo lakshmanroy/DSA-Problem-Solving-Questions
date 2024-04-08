1374fcc8c957965f3d21e26a053597f24b104bdd
Lakshman Roy <lkrd5115@gmail.com>
2024-04-08 20:50:03 +0530

    commit

--- bit Manipilation/bit/GetIthBit.java
+++ bit Manipilation/bit/GetIthBit.java
@@ -13,9 +13,15 @@ public class GetIthBit {
         return n| bitMask;
         
     }
+    public static int clearIthBit(int n,int i){
+        int bitMask= ~(1<<i);
+        return n&bitMask;
+    }
     public static void main(String[] args) {
         // System.out.println(getIthBit(10, 3));
-        System.out.println(setIthBit(10, 2));
+        // System.out.println(setIthBit(10, 2));
+
+        System.out.println(clearIthBit(10, 1));
         
     }
 }
