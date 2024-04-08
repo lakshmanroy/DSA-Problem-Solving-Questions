8fccf372bce4e048389061149066c098febdf1d8
Lakshman Roy <lkrd5115@gmail.com>
2024-04-08 18:01:37 +0530

    commit

--- bit Manipilation/bit/GetIthBit.java
+++ bit Manipilation/bit/GetIthBit.java
@@ -8,8 +8,14 @@ public class GetIthBit {
             return 1;
         }
     }
+    public static int setIthBit(int n,int i){
+        int bitMask=1<<i;
+        return n| bitMask;
+        
+    }
     public static void main(String[] args) {
-        System.out.println(getIthBit(10, 3));
+        // System.out.println(getIthBit(10, 3));
+        System.out.println(setIthBit(10, 2));
         
     }
 }
