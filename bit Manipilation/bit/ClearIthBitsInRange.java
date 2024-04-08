218ff27151e55c6b9eaeaf195b3b7843175f886d
Lakshman Roy <lkrd5115@gmail.com>
2024-04-08 22:00:46 +0530

    commit

--- bit Manipilation/bit/ClearIthBitsInRange.java
+++ bit Manipilation/bit/ClearIthBitsInRange.java
@@ -1,4 +1,4 @@
-public class ClearIthBit {
+public class ClearIthBitsInRange {
     public static int clearIthBitsInRange(int n,int i,int j){
         int a= (~0)<<(j+1);
         int b= (1<<i)-1;
---
+++ bit Manipilation/bit/PowerOfTwo.java
@@ -0,0 +1,9 @@
+public class PowerOfTwo {
+    public static boolean isPowerOfTwo(int n){
+        return (n&(n-1))==0;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(4));
+        
+    }
+}
