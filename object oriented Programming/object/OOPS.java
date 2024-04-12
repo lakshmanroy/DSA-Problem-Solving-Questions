6665e36a939c8ef8987ba7a7bef0be6095b482f5
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-12 20:51:55 +0530

    commit

---
+++ object oriented Programming/object/OOPS.java
@@ -0,0 +1,38 @@
+
+// package object oriented Programming;
+import java.util.*;
+
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.color = "Yellow";
+        System.out.println(p1.color);
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float Percentage;
+
+    void calPercentage(int phy, int chem, int math) {
+        Percentage = (phy + chem + math) / 3;
+    }
+}
