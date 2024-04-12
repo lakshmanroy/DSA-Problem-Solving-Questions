4ea8233ee16d8afc6c8f5d2dd06f4baf4e8dcaaa
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-12 21:12:32 +0530

    commit

---
+++ object oriented Programming/object/AccesModifier.java
@@ -0,0 +1,19 @@
+// package object oriented Programming.object;
+
+public class AccesModifier {
+    public static void main(String[] args) {
+        BankAccount myAcc = new BankAccount();
+        myAcc.userName = "Lakshman";
+        myAcc.setPassword("abcdefghi");
+    }
+}
+
+class BankAccount {
+    public String userName;
+    private String Password;
+
+    public void setPassword(String pwd) {
+        Password = pwd;
+
+    }
+}
