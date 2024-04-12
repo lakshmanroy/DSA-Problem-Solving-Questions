b67aebfe0ce1159dada8d9e7a3838e467b6567ac
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-12 21:33:09 +0530

    commit

---
+++ object oriented Programming/object/Getters_Setters.java
@@ -0,0 +1,32 @@
+// package object oriented Programming.object;
+
+public class Getters_Setters {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+        p1.setTip(10);
+        System.out.println(p1.getTip());
+    }
+}
+
+class Pen {
+    String color;
+    int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor) {
+        color = newColor;
+    }
+
+    void setTip(int newTip) {
+        tip = newTip;
+    }
+}
