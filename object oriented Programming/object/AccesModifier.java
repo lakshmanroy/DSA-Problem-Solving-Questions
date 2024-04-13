e76bf5a82b41fce050b90957933c97948eefa55e
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-13 07:36:22 +0530

    commit

--- object oriented Programming/object/AccesModifier.java
+++ object oriented Programming/object/AccesModifier.java
@@ -14,6 +14,5 @@ class BankAccount {
 
     public void setPassword(String pwd) {
         Password = pwd;
-
     }
 }
---
+++ object oriented Programming/object/Copy_Constructor.java
@@ -0,0 +1,51 @@
+public class Copy_Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Riya";
+        s1.roll = 453;
+        s1.password = "abcd";
+
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
+
+        Student s2 = new Student(s1);
+        s2.password = "xyz";
+        s1.marks[2] = 100;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(s2.marks[i] + " ");
+        }
+
+    }
+
+}
+
+class Student {
+    String name;
+    int roll;
+    String password;
+    int marks[];
+
+    Student(Student s1) {
+        marks = new int[3];
+        this.name = s1.name;
+        this.roll = s1.roll;
+        this.marks = s1.marks;
+    }
+
+    Student() {
+        marks = new int[3];
+        System.out.println("String are constructor...");
+    }
+
+    Student(String name) {
+        marks = new int[3];
+        this.name = name;
+    }
+
+    Student(int roll) {
+        marks = new int[3];
+        this.roll = roll;
+    }
+}
