caec44dd815277c27a453e453e2fcf7e70067ed8
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-13 22:12:12 +0530

    commit

--- object oriented Programming/object/Abstract_Classes.java
+++ object oriented Programming/object/Abstract_Classes.java
@@ -1,17 +1,28 @@
 public class Abstract_Classes {
     public static void main(String[] args) {
-        Horse h = new Horse();
-        h.eat();
-        h.walk();
+        // Horse h = new Horse();
+        // h.eat();
+        // h.walk();
 
-        Chicken c = new Chicken();
-        c.eat();
-        c.walk();
+        // // Chicken c = new Chicken();
+        // // c.eat();
+        // // c.walk();
+        // System.out.println(h.color);
+
+        Mustag m = new Mustag();
+        // Animals->Horse->Mustag
     }
 
 }
 
 abstract class Animals {
+    String color;
+
+    Animals() {
+        // color = "brown";
+        System.out.println("Animals constructor called");
+    }
+
     void eat() {
         System.out.println("Animals eat.");
     }
@@ -20,12 +31,30 @@ abstract class Animals {
 }
 
 class Horse extends Animals {
+    Horse() {
+        System.out.println("Horse constructor called");
+    }
+
+    void changeColor() {
+        color = "yellow";
+    }
+
     void walk() {
         System.out.println("walks on 4 legs");
     }
 }
 
+class Mustag extends Horse {
+    Mustag() {
+        System.out.println("Mustag constructor called");
+    }
+}
+
 class Chicken extends Animals {
+    void changeColor() {
+        color = "Red";
+    }
+
     void walk() {
         System.out.println("walks on 2 leg");
     }
