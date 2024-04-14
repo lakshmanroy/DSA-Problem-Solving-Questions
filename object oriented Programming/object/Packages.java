9526b0e0577c06053b2eab038525850d61a2b3b6
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-14 18:46:53 +0530

    commit

--- object oriented Programming/object/Packages.java
+++ object oriented Programming/object/Packages.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class Packages {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println(a);
+        String name = sc.next();
+        System.out.println(name);
+
     }
 }
