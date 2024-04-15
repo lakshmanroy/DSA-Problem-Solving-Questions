7fcbb9595e446f7f00c1167f060a9c8ce21452f8
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-15 21:26:09 +0530

    commit

--- 2D Arrays/DiagonalSum.java
+++ 2D Arrays/DiagonalSum.java
@@ -1,36 +1,35 @@
 public class DiagonalSum {
-    public static int DigonalSums(int matrix[][]){
-         int sum=0;
-        // for(int i=0;i<matrix.length;i++){
-        //     for(int j=0;j<matrix[0].length;j++){
-        //         if (i==j) {
-        //             sum+= matrix[i][j];
-        //         }
-        //         else if (i+j==matrix.length-1) {
-        //             sum+=matrix[i][j];
-                    
-        //         }
-        //     }
-        // }
-
-        // second matrix method
-        for(int i=0;i<matrix.length;i++){
-            //pd
-            sum+= matrix[i][i];
-            //sd
-            if (i !=matrix.length-1-i) {
-                sum+= matrix[i][matrix.length-i-1];
+    public static int DigonalSums(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                } else if (i + j == matrix.length - 1) {
+                    sum += matrix[i][j];
+                }
             }
         }
-        
+
+        // second matrix method
+        for (int i = 0; i < matrix.length; i++) {
+            // pd
+            sum += matrix[i][i];
+            // sd
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - i - 1];
+            }
+        }
+
         return sum;
     }
+
     public static void main(String[] args) {
-    int matrix[][]= {{1,2,3,4},
-                    {5,6,7,8},
-                    {9,10,11,12},
-                    {13,14,15,16}};
- System.out.print( DigonalSums(matrix));
+        int matrix[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+        System.out.print(DigonalSums(matrix));
 
     }
 }
