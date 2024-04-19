4b92cde87b9912d5c804af9e18c679a59ee3c65e
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-04-19 22:52:20 +0530

    commit

--- Recursion/TillingProblem.java
+++ Recursion/TillingProblem.java
@@ -3,11 +3,13 @@ public class TillingProblem {
         if (n == 0 || n == 1) {
             return 1;
         }
-        int fnm1 = tillingProblem(n - 1);
-        int fnm2 = tillingProblem(n - 2);
+        // int fnm1 = tillingProblem(n - 1);
+        // int fnm2 = tillingProblem(n - 2);
 
-        int totalWay = fnm1 + fnm2;
-        return totalWay;
+        // int totalWay = fnm1 + fnm2;
+        // return totalWay;
+        return tillingProblem(n - 1) + tillingProblem(n - 2);
+
     }
 
     public static void main(String[] args) {
