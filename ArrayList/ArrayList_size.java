819ea6a3fdabb26bbe1c87727c63d654429f4f31
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-20 23:26:07 +0530

    commit

---
+++ ArrayList/ArrayList_size.java
@@ -0,0 +1,22 @@
+// package ArrayList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayList_size {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+    }
+}
