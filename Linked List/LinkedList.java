9da8da8597240fc8f554c101f7dd579d4f4d07ef
Lakshman kumar <lakshmankumar.it2021@dscet.ac.in>
2024-05-29 15:04:30 +0530

    commit

--- Linked List/LinkedList.java
+++ Linked List/LinkedList.java
@@ -1,22 +1,22 @@
-// package Linked List;
 
 public class LinkedList {
-    public static class Node{
+    public static class Node {
         int data;
         Node next;
 
-        public Node(int data){
-            this.data=data;
-            this.next=null;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.head= new Node(1);
-        ll.head.next= new Node(2);
+        ll.head = new Node(1);
+        ll.head.next = new Node(2);
 
     }
 }
