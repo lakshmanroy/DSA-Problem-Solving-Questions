621df723d8f4cead76c141784129282d83c07a40
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-06-01 00:12:41 +0530

    commit

---
+++ Linked List/PrintLinkedList.java
@@ -0,0 +1,60 @@
+public class PrintLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void AddFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void AddLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        if (head == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        PrintLinkedList ll = new PrintLinkedList();
+        ll.print();
+        ll.AddFirst(2);
+        ll.print();
+        ll.AddFirst(1);
+        ll.print();
+        ll.AddLast(3);
+        ll.print();
+        ll.AddLast(4);
+        ll.print();
+    }
+}
