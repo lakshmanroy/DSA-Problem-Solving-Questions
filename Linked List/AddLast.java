716c026856ac980bba7e35c110f673752b182060
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-31 23:39:40 +0530

    commit

---
+++ Linked List/AddLast.java
@@ -0,0 +1,31 @@
+public class AddLast {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public static void last(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public static void main(String[] args) {
+        AddLast ll = new AddLast();
+        ll.last(3);
+        ll.last(4);
+
+    }
+}
