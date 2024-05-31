fa93f70739741af07dcd191ab8369beb5f95ec1a
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-05-31 22:57:29 +0530

    commit

--- Linked List/AddFirst.java
+++ Linked List/AddFirst.java
@@ -14,24 +14,20 @@ public class AddFirst {
     public static Node head;
     public static Node tail;
 
-    public void addFirst(int data) {
-        // step1- create new node
+    public void first(int data) {
         Node newNode = new Node(data);
         if (head == null) {
             head = tail = newNode;
             return;
         }
-        // step2- newNode next = head
-        newNode.next = head; // link
-
-        // step3- head= newNode
+        newNode.next = head;
         head = newNode;
     }
 
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        ll.addFirst(1);
-        ll.addFirst(2);
+        AddFirst ll = new AddFirst();
+        ll.first(1);
+        ll.first(2);
 
     }
 }
