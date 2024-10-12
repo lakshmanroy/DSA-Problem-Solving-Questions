a0d3563c253db08eb4f53fa381e9e2b9ce8e70e3
lakshmanroy <121234657+lakshmanroy@users.noreply.github.com>
2024-10-12 23:48:43 +0530

    commit

---
+++ LinkedList_part2.java/CollectionFramework.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+public class CollectionFramework {
+    public static void main(String[] args) {
+        LinkedList<Integer>ll=new LinkedList<>();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addFirst(0);
+
+        System.out.println(ll);
+        ll.removeLast();
+        ll.removeFirst();
+
+        System.out.println(ll);
+
+    }
+    
+}
