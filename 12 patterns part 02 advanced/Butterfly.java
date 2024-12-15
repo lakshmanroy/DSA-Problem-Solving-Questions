e9aff5c4ac46beda45f21d1663d8e67b0475050a
Lakshman kumar <121234657+lakshmanroy@users.noreply.github.com>
2024-12-15 23:59:45 +0530

    Update Butterfly.java

--- 12 patterns part 02 advanced/Butterfly.java
+++ 12 patterns part 02 advanced/Butterfly.java
@@ -1,6 +1,7 @@
 public class Butterfly {
     public static void Butterflys(int n){
     //1st half
+        
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
